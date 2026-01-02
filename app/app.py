d6c94cad31abe1d196d7ca1fa3523562d6fb49f8
Mehraj Patel <mehrajpatel13@gmail.com>
2026-01-02 17:39:29 +0530

    Re-add app.py without secrets

---
+++ app/app.py
@@ -0,0 +1,12 @@
+cat <<EOF > app/app.py
+import os
+
+API_KEY = os.environ.get("API_KEY")
+
+def connect():
+    password = os.environ.get("DB_PASSWORD")
+    print("Connecting securely")
+
+if __name__ == "__main__":
+    connect()
+EOF
