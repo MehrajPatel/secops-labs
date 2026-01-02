5675cd055f4440a5cd68e4f1fd4f4fd6eb0c12ab
Mehraj Patel <mehrajpatel13@gmail.com>
2026-01-02 19:58:20 +0530

    test

--- app/app.py
+++ app/app.py
@@ -2,12 +2,12 @@ import os
 
 def get_user(user_id):
     query = "SELECT * FROM users WHERE id=" + user_id
-    print("Executing query:", query)
+
     return query
 
 def connect():
     password = os.environ.get("DB_PASSWORD")
-    print("Connecting securely")
+
 
 if __name__ == "__main__":
     connect()
