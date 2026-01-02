8a9803652ca470a81d69655a9b252e06403c898b
Mehraj Patel <mehrajpatel13@gmail.com>
2026-01-02 18:25:30 +0530

    Introduce insecure SQL query to validate Semgrep enforcement

--- app/app.py
+++ app/app.py
@@ -1,6 +1,9 @@
 import os
 
-API_KEY = os.environ.get("API_KEY")
+def get_user(user_id):
+    query = "SELECT * FROM users WHERE id=" + user_id
+    print("Executing query:", query)
+    return query
 
 def connect():
     password = os.environ.get("DB_PASSWORD")
@@ -8,4 +11,4 @@ def connect():
 
 if __name__ == "__main__":
     connect()
-
+    get_user("123")
